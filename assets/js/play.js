16554b1b092a5d316ea1c15384dde8a8daab4f64
sherryrich <ricsherry@gmail.com>
2022-04-11 19:22:07 +0000

    declared variables

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,3 +1,5 @@
+//Modal - how to play
+
 const open = document.getElementById("open");
 const modal_container = document.getElementById("modal_container");
 const close =  document.getElementById("close");
@@ -9,3 +11,17 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     modal_container.classList.remove("show");
 });
+
+// Main JS - Declare const variables for DOM elements 
+const question = document.querySelector("#question")
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector("#progressText")
+const scoreText = document.querySelector("#score")
+const progressBarFull = document.querySelector("#progressBarFull")
+
+// Declare variables for game
+let currentQuestion ={};
+let acceptingAnswers = true;
+let scor = 0;
+let questionCounter = 0;
+let availableQuestions = [];
