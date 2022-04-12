024c5c69f749814720edce519f29932535de2c36
sherryrich <ricsherry@gmail.com>
2022-04-12 19:48:01 +0000

    updated topscores.js and topscores.html to display topscores correctly

--- assets/js/topscores.js
+++ assets/js/topscores.js
@@ -2,6 +2,6 @@ const topScoresList = document.querySelector("#topScoresList")
 const topScores = JSON.parse(localStorage.getItem("topScores")) || []
 
 topScoresList.innerHTML =
-topScores.MAP(score => {
-    return `<li class="top-score">${score.name} - ${score.score}</li>`
+topScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join("")
