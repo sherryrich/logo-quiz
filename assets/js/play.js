32ee3c02377495eab8a8a58d00218d5d41bcc71d
sherryrich <ricsherry@gmail.com>
2022-04-13 07:45:51 +0000

    updated css on mobile devices

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,3 +1,4 @@
+
 // Main JS - Declare const variables for DOM elements 
 const question = document.querySelector("#question")
 const choices = Array.from(document.querySelectorAll('.choice-text'));
@@ -6,14 +7,15 @@ const scoreText = document.querySelector("#score")
 const progressBarFull = document.querySelector("#progressBarFull")
 
 // Declare variables for game
-let currentQuestion = {}
+let currentQuestion ={}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 // questions in game
-let questions = [{
+let questions = [
+    {
         question: "assets/images/logo7.PNG",
         choice1: "BP",
         choice2: "ExxonMobil",
@@ -74,9 +76,9 @@ getNewQuestion = () => {
     progressText.innerText = `Question ${questionCounter} of ${max_questions}`
 
     //Update the progress any time the user answer a question
-    progressBarFull.style.width = `${(questionCounter / max_questions) * 100}%` // calculate what question user is on and makes a percentage
+      progressBarFull.style.width = `${(questionCounter / max_questions) * 100}%` // calculate what question user is on and makes a percentage
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length) 
     currentQuestion = availableQuestions[questionsIndex] // keeps track of what question user is currently on
     question.src = currentQuestion.question
 
