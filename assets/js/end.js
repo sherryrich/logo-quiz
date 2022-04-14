9b692977b312887d1676ffd2736be141ea982fb9
sherryrich <ricsherry@gmail.com>
2022-04-14 10:11:45 +0000

    updating end.js with semicolon to pass JSHint validation

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,33 +1,33 @@
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const topScores = JSON.parse(localStorage.getItem("topScores")) || []
+const topScores = JSON.parse(localStorage.getItem("topScores")) || [];
 
-const max_high_scores = 5
+const max_high_scores = 5;
 
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-    saveScoreBtn.disabled = !username.value
-})
+    saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
 
     const score = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
-    topScores.push(score)
+    topScores.push(score);
 
     topScores.sort((a, b) => {
-        return b.score - a.score
-    })
+        return b.score - a.score;
+    });
 
-    topScores.splice(5)
-    localStorage.setItem("topScores", JSON.stringify(topScores))
-    window.location.assign("/logo-quiz/topscores.html")
-}
+    topScores.splice(5);
+    localStorage.setItem("topScores", JSON.stringify(topScores));
+    window.location.assign("/topscores.html");
+};
