c1d01666b02cae6fdf8b33922c2c9e80ec3f1cf8
sherryrich <ricsherry@gmail.com>
2022-04-14 10:38:49 +0000

    updated topscores.js with missing semicolons

--- assets/js/topscores.js
+++ assets/js/topscores.js
@@ -1,7 +1,7 @@
-const topScoresList = document.querySelector("#topScoresList")
-const topScores = JSON.parse(localStorage.getItem("topScores")) || []
+const topScoresList = document.querySelector("#topScoresList");
+const topScores = JSON.parse(localStorage.getItem("topScores")) || [];
 
 topScoresList.innerHTML =
     topScores.map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`
-    }).join("")
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    }).join("");
