4575159f1a027a867d86ac93dec5dc9d481d6a2a
sherryrich <ricsherry@gmail.com>
2022-04-15 07:17:39 +0000

    Added greeting to homepage and greeting.js file

---
+++ assets/js/greeting.js
@@ -0,0 +1,19 @@
+/* This script shows a greeting to user based on users current time
+example taken from JavasSctrip jQuery book */
+
+let today = new Date(); // create a new date object
+let hourNow = today.getHours(); // find the current hour
+let greeting;
+
+//display the greetting based on users current time
+if (hourNow > 18)  {
+    greeting = "Good Evening & good luck";
+} else if (hourNow > 12) {
+    greeting = "Good Afternoon & good luck";
+} else if (hourNow > 0) {
+    greeting = "Good Morning & good luck";
+} else {
+    greeting = "Welcome & good luck";
+}
+let el = document.getElementById("greeting");
+el.textContent = greeting;
